133c1d073340ac5ef137885ceb2391fc59f163d4
HusseinElbendary <77048468+HusseinElbendary@users.noreply.github.com>
2021-01-06 12:20:47 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,13 @@ var audio = document.getElementById("autoplay");
 var audioButton = document.getElementById("playButton");
 var arrow=document.getElementById("arrow");
 var songPannel=document.getElementById("songPanel");
+var socialMedia=document.getElementsByClassName("socialMedia");
+var navLinks=document.getElementsByClassName("navLinks");
 var timeOut;
 hideSong();
+FooterSize();
+headerSize();
+
 
 audio.pause();
 // var t;
@@ -36,18 +41,18 @@ function toggleMusic()
 }
 function showSong()
 {
-// songPannel.innerText="Celeste OST: Resurrections";
 
-if(window.innerWidth>625){
-songPannel.style.padding="2px  5px 38px  254px";
-songPannel.style.marginTop= "13px";
 
-timeOut=setTimeout(function() {
-	songPannel.style.transition="none";
-	songPannel.innerText="Celeste OST: Resurrections";
-	songPannel.style.padding="2px  5px 2px  60px"; }
-	,4000);
-}
+	if(window.innerWidth>625){
+	songPannel.style.padding="2px  5px 38px  254px";
+	songPannel.style.marginTop= "13px";
+
+	timeOut=setTimeout(function() {
+		songPannel.style.transition="none";
+		songPannel.innerText="Celeste OST: Resurrections";
+		songPannel.style.padding="2px  5px 2px  60px"; }
+		,4000);
+	}
 }
 function hideSong()
 {
@@ -62,6 +67,41 @@ function hideSong()
 	,15);
 	
 }
+
+function FooterSize(){
+	if(window.innerWidth<575)
+	 {
+	 	for(var i=0;i<4;i++)
+	 	{
+	 	socialMedia[i].style.width="40px";
+	 	}
+	 }
+	 else{
+	 	for(var i=0;i<4;i++)
+	 	{
+	 	socialMedia[i].style.width="75px";
+	 	}
+	 }
+}
+
+function headerSize(){
+	if(window.innerWidth<575)
+	{
+		navLinks[0].style.fontSize="2rem";
+	}
+	else{
+		navLinks[0].style.fontSize="3rem";
+	}
+
+}
+
+function resizeElements()
+{
+	FooterSize();
+	headerSize();
+}
+
 audioButton.addEventListener("click",toggleMusic);
 audioButton.addEventListener("mouseenter",showSong);
 audioButton.addEventListener("mouseleave",hideSong);
+window.addEventListener("resize",resizeElements);
